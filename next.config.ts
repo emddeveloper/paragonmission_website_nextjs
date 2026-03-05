2e734a0293c2c74ee3ff166cf9c33a05f84c21ee
emddeveloper <emd.developer@gmail.com>
2026-03-06 00:09:37 +0530

    5-3-2026 SK Aminul Reverting the next.config changes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "paragonmission.com",
-        pathname: "/wp-content/uploads/**",
-      },
-    ],
-  },
+   images: {
+      domains: ["paragonmission.com"],
+   },
 };
 
 export default nextConfig;
-
